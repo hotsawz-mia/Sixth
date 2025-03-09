c8bc45e5de1f0da3310229b35c0ccf12e8ae49db
hotsawz-mia <limiter_inks.0k@icloud.com>
2025-03-09 10:24:31 +0000

    Second commit with working app

--- Sixth/ContentView.swift
+++ Sixth/ContentView.swift
@@ -7,15 +7,36 @@
 
 import SwiftUI
 
+struct Todo: Codable {
+    let title: String
+}
+
 struct ContentView: View {
+    @State private var data: String = "Loading..."
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Sixth")
+            Text(data)
+                .padding()
+            Button("Fetch Data") {
+                fetchData()
+            }
         }
         .padding()
+    }
+    func fetchData() {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/todos/1") else { return }
+
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let data = data, let todo = try? JSONDecoder().decode(Todo.self, from: data) {
+                DispatchQueue.main.async {
+                    self.data = todo.title
+                }
+            }
+        }.resume()
     }
 }
 
