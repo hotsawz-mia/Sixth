959bbdab9dc30a5fdfa624de7768a8265d74fbf8
hotsawz-mia <limiter_inks.0k@icloud.com>
2025-03-09 08:12:38 +0000

    Initial Commit

---
+++ Sixth/SixthApp.swift
@@ -0,0 +1,17 @@
+//
+//  SixthApp.swift
+//  Sixth
+//
+//  Created by Mark James on 09/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct SixthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
